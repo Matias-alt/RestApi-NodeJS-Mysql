13b19aed1593a143f21532eba833925ed4b82e7c
Matias-alt <mat.altamirano.22@gmail.com>
2021-04-02 20:25:45 -0300

    create new endpoints

--- src/routes/products.js
+++ src/routes/products.js
@@ -15,6 +15,36 @@ router.get('/products', (req, res) =>{
     });
 });
 
+router.get('/products/:id', (req, res) =>{
+
+    const { id } = req.params;
+
+    mysqlConnection.query('SELECT * FROM bsale_test.product WHERE id = ?', [id], (err, rows, fields) =>{
+        
+        if(!err){
+            res.json(rows);
+        }
+        else{
+            console.log("Error query: ", err);
+        }          
+    });
+});
+
+router.get('/products_bycategory/:category', (req, res) =>{
+
+    const { category } = req.params;
+
+    mysqlConnection.query('SELECT * FROM bsale_test.product WHERE category = ?', [category], (err, rows, fields) =>{
+        
+        if(!err){
+            res.json(rows);
+        }
+        else{
+            console.log("Error query: ", err);
+        }          
+    });
+    
+});
 
 
 module.exports = router;
